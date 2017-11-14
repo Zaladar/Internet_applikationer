29892b02473d30f68a28c378b30ba99e0c05f649
Zaladar <oscarjwhaglund@gmail.com>
2017-11-14 18:37:21 +0100

    vacum

--- Model/login.php
+++ Model/login.php
@@ -2,7 +2,7 @@
    include 'init/config.php';
    session_start();
    echo("its working sortof");
-
+  
    if($_SERVER["REQUEST_METHOD"] == "POST") {
       // username and password sent from form
         //$myusername = mysqli_real_escape_string($conn,$_POST['username']);
@@ -14,10 +14,8 @@
         //$active = $row['active'];
         //echo("$result");
         //$count = mysqli_num_rows($result);
-        echo("DIS BORKED 1");
-
         // If result matched $myusername and $mypassword, table row must be 1 row
-
+/*
         if($count == 1) {
           echo("DIS BORKED 2");
            session_register("myusername");
@@ -31,7 +29,7 @@
            $error = "Your Login Name or Password is invalid";
            $conn->close();
            header("location: index.php");
-        }
+        }*/
    }
 
 ?>
