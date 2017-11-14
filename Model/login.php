a3968792bfb3aef71bda2bc1f4eb6605b02c23d3
Zaladar <oscarjwhaglund@gmail.com>
2017-11-14 17:34:28 +0100

    vacum

--- Model/login.php
+++ Model/login.php
@@ -3,13 +3,13 @@
    session_start();
    if (mysqli_connect_errno())
   {
-  echo "Failed to connect to MySQL: "
+  echo ()"Failed to connect to MySQL: ");
   }
    if($_SERVER["REQUEST_METHOD"] == "POST") {
       // username and password sent from form
         /*$myusername = mysqli_real_escape_string($conn,$_POST['username']);
         $mypassword = mysqli_real_escape_string($conn,$_POST['password']);*/
-        echo("$_POST['username']")
+        echo("$_POST['username']");
         $sql = "SELECT username FROM user WHERE username = '$_POST['username']' and password = '$_POST['password']'";
         $result = $conn->query($sql);
         $result->num_rows;
