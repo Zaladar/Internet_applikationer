f9ac0287850b47fab22c3c1db58a6eaa1fdb35c3
Zaladar <oscarjwhaglund@gmail.com>
2017-11-14 14:35:31 +0100

    tryin

--- Model/init/config.php
+++ Model/init/config.php
@@ -3,5 +3,5 @@
   $username = "root";
   $password = "niclas91";
   $database = "users"
-  $conn = new mysqli($servername, $username, $password);
+  $conn = new mysqli($servername, $username, $password $database);
 ?>
