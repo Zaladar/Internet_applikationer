7b2e67c56ebd13c99abca2ded2548c7a99f6f1ae
Zaladar <oscarjwhaglund@gmail.com>
2017-11-14 15:51:36 +0100

    vacum

--- Model/login.php
+++ Model/login.php
@@ -3,7 +3,7 @@
    session_start();
    if($_SERVER["REQUEST_METHOD"] == "POST") {
       // username and password sent from form
-
+      echo("DIS BORKED 1");
       $myusername = mysqli_real_escape_string($conn,$_POST['username']);
       $mypassword = mysqli_real_escape_string($conn,$_POST['password']);
 
@@ -17,12 +17,14 @@
       // If result matched $myusername and $mypassword, table row must be 1 row
 
       if($count == 1) {
+          echo("DIR BORKED 2")
          session_register("myusername");
          $_SESSION['login_user'] = $myusername;
          $conn->close();
          $_SESSION['lol'] = 'yes';
          header("location: index.php");
       }else {
+          echo("DIR DIDNT BORKED 2")
          $error = "Your Login Name or Password is invalid";
          $conn->close();
          header("location: index.php");
