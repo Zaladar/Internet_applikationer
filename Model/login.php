e6a52026b40de55e9e19aaaaeddf16eb4cef2ef9
Zaladar <oscarjwhaglund@gmail.com>
2017-11-14 16:27:30 +0100

    vacum

--- Model/login.php
+++ Model/login.php
@@ -3,27 +3,32 @@
    session_start();
    if($_SERVER["REQUEST_METHOD"] == "POST") {
       // username and password sent from form
-        $myusername = mysqli_real_escape_string($conn,$_POST['username']);
-        $mypassword = mysqli_real_escape_string($conn,$_POST['password']);
+        /*$myusername = mysqli_real_escape_string($conn,$_POST['username']);
+        $mypassword = mysqli_real_escape_string($conn,$_POST['password']);*/
 
-        $sql = "SELECT username FROM users WHERE username = '$myusername' and password = '$mypassword'";
-        $result = mysqli_query($db,$sql);
-        $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+        $sql = "SELECT username FROM site WHERE username = '$_POST['username']' and password = '$_POST['password']'";
+        $result = $conn->query($sql);
+        $row = $row = $result->fetch_assoc();
         $active = $row['active'];
 
         $count = mysqli_num_rows($result);
+        echo("DIS BORKED 1");
 
         // If result matched $myusername and $mypassword, table row must be 1 row
 
         if($count == 1) {
+          echo("DIS BORKED 2");
            session_register("myusername");
            $_SESSION['login_user'] = $myusername;
            $conn->close();
            $_SESSION['lol'] = 'yes';
            header("location: index.php");
-        }else {
+        }
+
+        else {
+          echo("DIS didnt BORKED 2");
+
            $error = "Your Login Name or Password is invalid";
-           echo ($error)
            $conn->close();
            header("location: index.php");
         }*/
