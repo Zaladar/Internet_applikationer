411d2d396da8c0963beaec5d9d605702f7104f06
Zaladar <oscarjwhaglund@gmail.com>
2017-11-14 17:55:06 +0100

    vacum

--- Model/login.php
+++ Model/login.php
@@ -1,12 +1,12 @@
 <?php
-   include 'Init/config.php';
+   include 'init/config.php';
    session_start();
    echo("its working sortof");
    if($_SERVER["REQUEST_METHOD"] == "POST") {
       // username and password sent from form
         /*$myusername = mysqli_real_escape_string($conn,$_POST['username']);
         $mypassword = mysqli_real_escape_string($conn,$_POST['password']);*/
-        /*echo("$_POST['username']");
+        echo("$_POST['username']");
         /*$sql = "SELECT username FROM user WHERE username = '$_POST['username']' and password = '$_POST['password']'";
         $result = $conn->query($sql);
         $row = $result->fetch_assoc();
