6a34db41ab642090617f10a7a7122dd0eefa6680
Zaladar <oscarjwhaglund@gmail.com>
2017-11-14 17:53:01 +0100

    vacum

--- Model/init/config.php
+++ Model/init/config.php
@@ -3,9 +3,8 @@
   $username = "root";
   $password = "niclas91";
   $database = "users"
-  $conn = new mysqli($servername, $username, $password $database);
-
-  /*if ($conn->connect_error) {
+  $conn = new mysqli($servername, $username, $password, $database);
+  *if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-}*/
+  }
 ?>
--- Model/login.php
+++ Model/login.php
@@ -1,10 +1,12 @@
 <?php
-   include 'init/config.php';
+   include 'Init/config.php';
    session_start();
+   echo("its working sortof");
    if($_SERVER["REQUEST_METHOD"] == "POST") {
       // username and password sent from form
         /*$myusername = mysqli_real_escape_string($conn,$_POST['username']);
         $mypassword = mysqli_real_escape_string($conn,$_POST['password']);*/
+        echo("$_POST['username']");
         /*$sql = "SELECT username FROM user WHERE username = '$_POST['username']' and password = '$_POST['password']'";
         $result = $conn->query($sql);
         $row = $result->fetch_assoc();
@@ -16,13 +18,17 @@
         // If result matched $myusername and $mypassword, table row must be 1 row
 
         if($count == 1) {
+          echo("DIS BORKED 2");
            session_register("myusername");
            $_SESSION['login_user'] = $myusername;
+           $conn->close();
            header("location: index.php");
         }
 
         else {
-          $error = "Your Login Name or Password is invalid";
+          echo("DIS didnt BORKED 2");
+           $error = "Your Login Name or Password is invalid";
+           $conn->close();
            header("location: index.php");
         }*/
    }
