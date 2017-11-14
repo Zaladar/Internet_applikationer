12438dc87d63bff194e78fef12d891005665acf9
Zaladar <oscarjwhaglund@gmail.com>
2017-11-14 18:44:05 +0100

    vacum

--- Model/login.php
+++ Model/login.php
@@ -10,6 +10,7 @@
         //echo ("$_POST['username']");
         $sql = "SELECT username FROM user WHERE username = '$myusername' and password = '$mypassword'";
         $result = $conn->query($sql);
+        echo("$result")
         //$row = $result->fetch_assoc();
         //$active = $row['active'];
         //echo("$result");
