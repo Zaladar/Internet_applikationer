05459a484e2eb4429043b7ae5070a3f4a8fd317f
Zaladar <oscarjwhaglund@gmail.com>
2017-11-14 14:31:52 +0100

    tryin

--- Model/init/config.php
+++ Model/init/config.php
@@ -1,12 +1,7 @@
 <?php
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'niclas91');
-define('DB_DATABASE', 'site');
-
-$conn = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE)
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+  $servername = "localhost";
+  $username = "root";
+  $password = "niclas91";
+  $database = "users"
+  $conn = new mysqli($servername, $username, $password);
 ?>
