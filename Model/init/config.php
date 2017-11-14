b2127e0b334da3445dda4e97b102e846116a422e
Zaladar <oscarjwhaglund@gmail.com>
2017-11-14 17:57:17 +0100

    vacum

--- Model/init/config.php
+++ Model/init/config.php
@@ -4,7 +4,4 @@
   $password = "niclas91";
   $database = "site"
   $conn = new mysqli($servername, $username, $password, $database);
-  if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-  }
 ?>
