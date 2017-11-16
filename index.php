2c956c0b60b7bc7949b235e8c7aae5ab53adba4b
Zaladar <oscarjwhaglund@gmail.com>
2017-11-16 20:46:33 +0100

    vacum

--- index.php
+++ index.php
@@ -9,7 +9,6 @@
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <script> var logchk = '<?php echo $_SESSION['is_login']; ?>';</script>
-      <script src="/Script/loginclose.js"></script>
     </head>
     <body>
       <header>
@@ -70,5 +69,6 @@
         </div>
       </div>
       <script src="/Script/isloggedin.js"></script>
+      <script src="/Script/loginclose.js"></script>
    </body>
 </html>
