6f15f073d40fed96e25d2bf1d4d150c363fdbd21
Zaladar <oscarjwhaglund@gmail.com>
2017-11-16 16:17:12 +0100

    vacum

--- Script/loginclose.js
+++ Script/loginclose.js
@@ -1,9 +1,15 @@
 // Get the modal
-var modal = document.getElementById('login');
+var modal1 = document.getElementById('login');
+var modal2 = document.getElementById('register');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == modal1) {
+        modal1.style.display = "none";
+    }
+}
+window.onclick = function(event) {
+    if (event.target == modal2) {
+        modal2.style.display = "none";
     }
 }
