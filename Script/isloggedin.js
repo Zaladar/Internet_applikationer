136a5bb9be279b7847b38c08a732d2d1f920ad55
Zaladar <oscarjwhaglund@gmail.com>
2017-11-16 20:28:58 +0100

    vacum

--- Script/isloggedin.js
+++ Script/isloggedin.js
@@ -1,7 +1,7 @@
 //alert(session); this will make an alert message pop up.
 var loginbtn = document.getElementById('loginbtn');
 var logoutbtn = document.getElementById('logoutbtn');
-var registerbtn = document.getElementById('rebtn');
+var registerbtn = document.getElementById('regbtn');
 if (logchk==true){
     loginbtn.style.display = "none";
     registerbtn.style.display = "none";
