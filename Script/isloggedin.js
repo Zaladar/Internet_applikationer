957ea2f418887730c638545726f5183b945e00cd
Zaladar <oscarjwhaglund@gmail.com>
2017-11-16 13:06:51 +0100

    vacum

--- Script/isloggedin.js
+++ Script/isloggedin.js
@@ -1,8 +1,8 @@
-var session = (<?php echo $_SESSION['is_logged']?'true':'false'; ?>);
+var session = "<?php echo $_SESSION['is_logged']?'true':'false'; ?>";
 //alert(session); this will make an alert message pop up.
 var login = document.getElementById('login');
 var logout = document.getElementById('logout');
 if (session == true){
     login.style.display = "none";
-    logout.styel.display = "block";
+    logout.style.display = "block";
 }
