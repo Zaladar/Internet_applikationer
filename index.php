672a6ec381566751a7fc1a0b8a330487474509b9
Zaladar <oscarjwhaglund@gmail.com>
2017-11-16 14:12:16 +0100

    vacum

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
     <body>
       <header>
         <h1> Tasty Recipies</h1>
+        <div id="logchk" value="<?php echo($_SESSION['is_login'])?>" style="display:none"/>
       </header>
       <nav>
         <ul>
@@ -57,6 +58,5 @@
           </p>
         </div>
       </div>
-      <div id="logchk" value="<?php echo($_SESSION['is_login'])?>" style="display:none"/>
    </body>
 </html>
