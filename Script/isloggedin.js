cd99b3a7067fc6e128571681d9b55784e57c4ac5
Zaladar <oscarjwhaglund@gmail.com>
2017-11-16 14:38:57 +0100

    vacum

--- Script/isloggedin.js
+++ Script/isloggedin.js
@@ -1,8 +1,7 @@
-var session = document.getElementById('logchk').value;
 //alert(session); this will make an alert message pop up.
 var login = document.getElementById('loginbtn');
 var logout = document.getElementById('logoutbtn');
-if (session == true){
+if (logchk == true){
     login.style.display = "none";
     logout.style.display = "block";
 }
--- index.php
+++ index.php
@@ -12,6 +12,9 @@
       <link rel="stylesheet" href="/css/stylesheet.css">
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <script>
+        var logchk = <?php echo $_SESSION['is_login']; ?>
+      </script>
       <script src="/Script/loginclose.js"></script>
       <script src="/Script/isloggedin.js"></script>
     </head>
