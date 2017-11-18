97320c1263de61aea8fd4297fccc27c5f5ff6b14
Zaladar <oscarjwhaglund@gmail.com>
2017-11-18 22:51:39 +0100

    vacum

--- Model/register.php
+++ Model/register.php
@@ -1,10 +1,9 @@
 <?php
    include 'init/config.php';
    session_start();
-   echo("what dis");
+
    if($_SERVER["REQUEST_METHOD"] == "POST") {
       // username and password sent from form
-        echo("now dis?");
         $myusername = mysqli_real_escape_string($conn,$_POST['username']);
         $mypassword = mysqli_real_escape_string($conn,$_POST['password']);
         //echo ("$_POST['username']");
@@ -13,18 +12,17 @@
         //$row = $result->fetch_assoc();
         //$active = $row['active'];
         $count = $result->num_rows;
-        echo ($count);
         if($count == 0) {
-
           $sql = "INSERT INTO user (username, password) VALUES ('$myusername', '$mypassword')";
+          $conn->query($sql);
           $_SESSION['result'] = 'Account created';
           $conn->close();
-          //header("location: ../index.php");
+          header("location: ../index.php");
         }
         else {
            $_SESSION['result'] = "Your userame is already in use";
            $conn->close();
-           //header("location: ../index.php");
+           header("location: ../index.php");
         }
    }
 ?>
