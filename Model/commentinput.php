2ef34c8d87324635d34c0fa3ccae4ab4aae78537
Zaladar <oscarjwhaglund@gmail.com>
2017-11-18 22:44:30 +0100

    vacum

--- Model/commentinput.php
+++ Model/commentinput.php
@@ -1,7 +1,6 @@
 <?php
    include 'init/config.php';
    session_start();
-   echo("hello world");
    if($_SERVER["REQUEST_METHOD"] == "POST") {
       // username and password sent from form
         $myusername = mysqli_real_escape_string($conn,$_SESSION['login_user']);
@@ -10,8 +9,6 @@
         $sql = "INSERT INTO comments (username, recipie, comment) VALUES ('$myusername', '$myrecipie', '$mycomment')";
         $result = $conn->query($sql);
         $conn->close();
-        echo($_SESSION['login_user']);
-        echo($_POST['recipie']);
-        //header("location: ".$_POST['recipie'].".php");
+        header("location: ../View/Recipies/".$_POST['recipie'].".php");
    }
 ?>
