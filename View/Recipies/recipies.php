5b48b7c9d7a89969eede5ecffe5475f8783e72fd
Zaladar <oscarjwhaglund@gmail.com>
2017-11-18 22:26:05 +0100

    vacum

--- View/Recipies/recipies.php
+++ View/Recipies/recipies.php
@@ -19,23 +19,36 @@
           <li><a href="/index.php">Home</a></li>
           <li><a class="active" href="#">Recipies</a></li>
           <li><a href="/View/calendar.php">Calendar</a></li>
+          <li id="regbtn"><a href="#" onclick="document.getElementById('register').style.display='block'">Register</a></li>
           <li id="loginbtn"><a href="#" onclick="document.getElementById('login').style.display='block'">Login</a></li>
           <li id="logoutbtn"><a href="/Model/logout.php">Logout</a></li>
           <!--<li style="float:right"><a href="#about">About</a></li>-->
         </ul>
       </nav>
-      <br class="clr" />
+      <br class="clr"/>
+      <div id="register" class="modal">
+        <form class="modal-content animate" action="/Model/register.php" method="post">
+          <div class="container">
+            <label></label>
+            <input type="text" placeholder="Enter Username" name="username" required>
+            <label></label>
+            <input type="password" placeholder="Enter Password" name="password" required>
+            <button type="submit">Register</button>
+          </div>
+        </form>
+      </div>
       <div id="login" class="modal">
         <form class="modal-content animate" action="/Model/login.php" method="post">
           <div class="container">
             <label></label>
-            <input type="text" placeholder="Enter Username" name="uname" required>
-            <label></label>      <script src="/Script/loginclose.js"></script>
-
-            <input type="password" placeholder="Enter Password" name="psw" required>
+            <input type="text" placeholder="Enter Username" name="username" required>
+            <label></label>
+            <input type="password" placeholder="Enter Password" name="password" required>
             <button type="submit">Login</button>
           </div>
         </form>
+      </div>
+      <div>
       </div>
       <div>
         <div class="marg">
@@ -48,6 +61,7 @@
           </div>
         </div>
       </div>
+      <script src="/Script/loginclose.js"></script>
       <script src="/Script/isloggedin.js"></script>
    </body>
 </html>
