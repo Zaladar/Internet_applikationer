8a62629d94bb0cf2a7dcb8ec70263c32b44c6a94
Zaladar <oscarjwhaglund@gmail.com>
2017-11-18 19:16:55 +0100

    vacum

--- Model/commentinput.php
+++ Model/commentinput.php
@@ -11,7 +11,6 @@
         $result = $conn->query($sql);
         $conn->close();
         echo($_SESSION['login_user']);
-        //header("location: ../index.php");
+        header("location: ../index.php");
    }
-   $conn->close();
 ?>
