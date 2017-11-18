27cffaa43bf2c74b289482cee47b3f99741f8c30
Zaladar <oscarjwhaglund@gmail.com>
2017-11-18 21:29:29 +0100

    vacum

--- Model/comment.php
+++ Model/comment.php
@@ -7,14 +7,14 @@ $result = $conn->query($sql);
 
 while($row = $result->fetch_assoc()) {
   echo '<div class="comments">';
-    echo '<div class="ucomment">';
-        echo ('<small class="name">'.$row['username'].'</small>');
-        if($_SESSION['login_user']== $row['username']){
-          echo(' <form action="/Model/delete.php"><input type="hidden" name="id" value="'.$row['ID'].'"><button type="submit" style="float:right">Delete</button></>form><br/>');}
-        echo '<br class="clr"/>';
-        echo ('<p class="comment">'.$row['comment'].'</p>');
-    echo'</div>';
-echo'</div>';
+  echo '<div class="ucomment">';
+  echo ('<small class="name">'.$row['username'].'</small>');
+  if($_SESSION['login_user']== $row['username']){
+    echo(' <form action="/Model/delete.php"><input type="hidden" name="id" value="'.$row['ID'].'"><button type="submit" style="float:right">Delete</button></>form><br/>');}
+  echo '<br class="clr"/>';
+  echo ('<p class="comment">'.$row['comment'].'</p>');
+  echo'</div>';
+  echo'</div>';
 }
 $conn->close();
 ?>
