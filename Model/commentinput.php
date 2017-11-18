d38b75b1e4b00bd2d519b6aeb37864ef18123ce3
Zaladar <oscarjwhaglund@gmail.com>
2017-11-18 19:38:05 +0100

    vacum

--- Model/commentinput.php
+++ Model/commentinput.php
@@ -4,7 +4,7 @@
    echo("hello world");
    if($_SERVER["REQUEST_METHOD"] == "POST") {
       // username and password sent from form
-        $myusername = mysqli_real_escape_string($conn,$_POST['username']);
+        $myusername = mysqli_real_escape_string($conn,$_SESSION['login_user']);
         $myrecipie = mysqli_real_escape_string($conn,$_POST['recipie']);
         $mycomment = mysqli_real_escape_string($conn,$_POST['comment']);
         $sql = "INSERT INTO comments (username, recipie, comment) VALUES ('$myusername', '$myrecipie', '$mycomment')";
--- View/Recipies/meatballs.php
+++ View/Recipies/meatballs.php
@@ -5,10 +5,11 @@
       <link rel="stylesheet" href="/css/stylesheet.css">
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <script>
-        var logchk = '<?php echo $_SESSION['is_login']; ?>';
-      </script>
+      <script> var logchk = '<?php echo $_SESSION['is_login']; ?>';</script>
       <script src="/Script/loginclose.js"></script>
+      <script src="/Script/isloggedin.js"></script>
+      <script> var uname='<?php echo($_SESSION['login_user'])?>'</script>
+
    </head>
    <body>
     <header>
@@ -19,20 +20,31 @@
         <li><a href="/index.php">Home</a></li>
         <li><a href="/View/Recipies/recipies.php">Recipies</a></li>
         <li><a href="/View/calendar.php">Calendar</a></li>
+        <li id="regbtn"><a href="#" onclick="document.getElementById('register').style.display='block'">Register</a></li>
         <li id="loginbtn"><a href="#" onclick="document.getElementById('login').style.display='block'">Login</a></li>
         <li id="logoutbtn"><a href="/Model/logout.php">Logout</a></li>
         <!--<li style="float:right"><a href="#about">About</a></li>-->
       </ul>
     </nav>
     <br class="clr"/>
+    <div id="register" class="modal">
+      <form class="modal-content animate" action="/Model/register.php" method="post">
+        <div class="container">
+          <label></label>
+          <input type="text" placeholder="Enter Username" name="username" required>
+          <label></label>
+          <input type="password" placeholder="Enter Password" name="password" required>
+          <button type="submit">Register</button>
+        </div>
+      </form>
+    </div>
     <div id="login" class="modal">
       <form class="modal-content animate" action="/Model/login.php" method="post">
         <div class="container">
           <label></label>
-          <input type="text" placeholder="Enter Username" name="uname" required>
-          <label></label>      <script src="/Script/loginclose.js"></script>
-
-          <input type="password" placeholder="Enter Password" name="psw" required>
+          <input type="text" placeholder="Enter Username" name="username" required>
+          <label></label>
+          <input type="password" placeholder="Enter Password" name="password" required>
           <button type="submit">Login</button>
         </div>
       </form>
@@ -87,7 +99,6 @@
                 <label></label>
                 <input type="text" placeholder="Enter comment" name="comment" style="height 60 px" required>
                 <input type="hidden" name="recipie" value="meatballs">
-                <input type="hidden" name="username" value="<?php echo($_SESSION['login_user'])?>">
                 <button type="submit">Submit</button>
               </div>
             </form>
@@ -96,6 +107,5 @@
       </div>
     </div>
     <br class="clr"/>
-    <script src="/Script/isloggedin.js"></script>
   </body>
 </html>
