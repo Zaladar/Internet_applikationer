f769c8a6739b0948856ef6393736858b2333a687
Zaladar <oscarjwhaglund@gmail.com>
2017-11-18 19:03:51 +0100

    vacum

--- Model/commentinput.php
+++ Model/commentinput.php
@@ -8,8 +8,10 @@
         $myrecipie = mysqli_real_escape_string($conn,$_POST['recipie']);
         $mycomment = mysqli_real_escape_string($conn,$_POST['comment']);
         $sql = "INSERT INTO comments (username, recipie, comment) VALUES ('$myusername', '$myrecipie', '$mycomment')";
+        $result = $conn->query($sql);
         $conn->close();
-        echo($_SESSION['login_user'])
+        echo($result)
+        echo($_SESSION['login_user']);
         //header("location: ../index.php");
    }
 ?>
