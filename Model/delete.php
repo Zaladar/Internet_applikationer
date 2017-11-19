61154d40d316f44aa31b632c4c7f5bcaaf6a9689
Zaladar <oscarjwhaglund@gmail.com>
2017-11-19 14:10:49 +0100

    vacum

--- Model/delete.php
+++ Model/delete.php
@@ -1,10 +1,14 @@
 <?php
 include 'init/config.php';
 session_start();
+echo("success!");
 if($_SERVER["REQUEST_METHOD"] == "POST") {
+  echo "AGAIN!";
   $myid = mysqli_real_escape_string($conn,$_SESSION['id']);
   $sql = "DELETE FROM comments WHERE ID='$myid";
+  echo($myid);
   $conn->query($sql);
   $conn->close();
+  //header("location: ../View/Recipies/".$_POST['recipie'].".php");
 }
  ?>
