24a54fbeb53535d2898f26d5f425c258d463a3c7
Zaladar <oscarjwhaglund@gmail.com>
2017-11-19 14:21:30 +0100

    vacum

--- Model/delete.php
+++ Model/delete.php
@@ -2,12 +2,21 @@
 include 'init/config.php';
 session_start();
 echo("success!");
+/*if($_SERVER["REQUEST_METHOD"] == "POST") {
+   // username and password sent from form
+     $myusername = mysqli_real_escape_string($conn,$_SESSION['login_user']);
+     $myrecipie = mysqli_real_escape_string($conn,$_POST['recipie']);
+     $mycomment = mysqli_real_escape_string($conn,$_POST['comment']);
+     $sql = "INSERT INTO comments (username, recipie, comment) VALUES ('$myusername', '$myrecipie', '$mycomment')";
+     $result = $conn->query($sql);
+     $conn->close();
+     header("location: ../View/Recipies/".$_POST['recipie'].".php");*/
 if($_SERVER["REQUEST_METHOD"] == "POST") {
   echo "AGAIN!";
   $myid = mysqli_real_escape_string($conn,$_POST['id']);
   $sql = "DELETE FROM comments WHERE ID='$myid'";
   echo($myid);
-  $conn->query($sql);
+  $result =$conn->query($sql);
   $conn->close();
   //header("location: ../View/Recipies/".$_POST['recipie'].".php");
 }
