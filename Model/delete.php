d35048a10a1f180b292891a67cc189b9b67c0b63
Zaladar <oscarjwhaglund@gmail.com>
2017-11-19 15:00:26 +0100

    vacum

--- Model/delete.php
+++ Model/delete.php
@@ -2,15 +2,6 @@
 include 'init/config.php';
 session_start();
 echo("success!");
-/*if($_SERVER["REQUEST_METHOD"] == "POST") {
-   // username and password sent from form
-     $myusername = mysqli_real_escape_string($conn,$_SESSION['login_user']);
-     $myrecipie = mysqli_real_escape_string($conn,$_POST['recipie']);
-     $mycomment = mysqli_real_escape_string($conn,$_POST['comment']);
-     $sql = "INSERT INTO comments (username, recipie, comment) VALUES ('$myusername', '$myrecipie', '$mycomment')";
-     $result = $conn->query($sql);
-     $conn->close();
-     header("location: ../View/Recipies/".$_POST['recipie'].".php");*/
 if($_SERVER["REQUEST_METHOD"] == "POST") {
   echo "AGAIN!";
   $myid = mysqli_real_escape_string($conn,$_POST['id']);
--- Script/isloggedin.js
+++ Script/isloggedin.js
@@ -2,8 +2,10 @@
 var loginbtn = document.getElementById('loginbtn');
 var logoutbtn = document.getElementById('logoutbtn');
 var registerbtn = document.getElementById('regbtn');
+var comment = document.getElementById('comm')
 if (logchk==true){
     loginbtn.style.display = "none";
     registerbtn.style.display = "none";
     logoutbtn.style.display = "block";
+    comment.style.display = "inline-block"
 }
