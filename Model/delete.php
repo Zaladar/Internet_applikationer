3101590d6fa5db230455f6fdd357e7a048226ed1
Zaladar <oscarjwhaglund@gmail.com>
2017-11-19 14:16:59 +0100

    vacum

--- Model/delete.php
+++ Model/delete.php
@@ -5,7 +5,7 @@ echo("success!");
 if($_SERVER["REQUEST_METHOD"] == "POST") {
   echo "AGAIN!";
   $myid = mysqli_real_escape_string($conn,$_POST['id']);
-  $sql = "DELETE FROM comments WHERE ID='$myid";
+  $sql = "DELETE FROM comments WHERE ID='$myid'";
   echo($myid);
   $conn->query($sql);
   $conn->close();
