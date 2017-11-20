d464a9425b1fe5cbe086d7bcfe33adab9e808ea5
Zaladar <oscarjwhaglund@gmail.com>
2017-11-20 09:30:23 +0100

    vacum

--- Model/delete.php
+++ Model/delete.php
@@ -1,12 +1,10 @@
 <?php
 include 'init/config.php';
 session_start();
-echo("success!");
 if($_SERVER["REQUEST_METHOD"] == "POST") {
   echo "AGAIN!";
   $myid = mysqli_real_escape_string($conn,$_POST['id']);
   $sql = "DELETE FROM comments WHERE ID='$myid'";
-  echo($myid);
   $result =$conn->query($sql);
   $conn->close();
   header("location: ../View/Recipies/".$_POST['recipie'].".php");
