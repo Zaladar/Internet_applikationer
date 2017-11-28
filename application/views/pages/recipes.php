dde22419f5ff6222879b43571bf6346b8b504c9a
Zaladar <oscarjwhaglund@gmail.com>
2017-11-28 22:18:03 +0100

    CIframework installed

---
+++ application/views/pages/recipes.php
@@ -0,0 +1,36 @@
+<br class="clr"/>
+<div id="register" class="modal">
+  <form class="modal-content animate" action="/Model/register.php" method="post">
+    <div class="container">
+      <label></label>
+      <input type="text" placeholder="Enter Username" name="username" required>
+      <label></label>
+      <input type="password" placeholder="Enter Password" name="password" required>
+      <button type="submit">Register</button>
+    </div>
+  </form>
+</div>
+<div id="login" class="modal">
+  <form class="modal-content animate" action="/Model/login.php" method="post">
+    <div class="container">
+      <label></label>
+      <input type="text" placeholder="Enter Username" name="username" required>
+      <label></label>
+      <input type="password" placeholder="Enter Password" name="password" required>
+      <button type="submit">Login</button>
+    </div>
+  </form>
+</div>
+<div>
+</div>
+<div>
+  <div class="marg">
+    <div>
+      <h2>These are the current recipies!</h2>
+      <ul>
+        <li><a href="meatballs.php">Meatballs!</a> </li>
+        <li><a href="pancakes.php">Pancakes!</a> </li>
+      </ul>
+    </div>
+  </div>
+</div>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -10,11 +10,10 @@
     <nav>
       <ul>
         <li><a class="active" href="<?php echo base_url();?>">Home</a></li>
-        <li><a href="/View/Recipies/recipies.php">Recipies</a></li>
-        <li><a href="/View/calendar.php">Calendar</a></li>
+        <li><a href="<?php echo base_url();?>/recipes">Recipies</a></li>
+        <li><a href="<?php echo base_url();?>/calendar">Calendar</a></li>
         <li id="regbtn"><a href="#" onclick="document.getElementById('register').style.display='block'">Register</a></li>
         <li id="loginbtn"><a href="#" onclick="document.getElementById('login').style.display='block'">Login</a></li>
         <li id="logoutbtn"><a href="/Model/logout.php">Logout</a></li>
-        <!--<li style="float:right"><a href="#about">About</a></li>-->
       </ul>
     </nav>
