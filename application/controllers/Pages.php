8dce48329a08aa7f3db3ac1ccbc1322df6deecb0
Zaladar <oscarjwhaglund@gmail.com>
2017-11-28 17:44:11 +0100

    CIframework installed

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -7,7 +7,7 @@
         $data['title'] = ucfirst($page);
 
         $this->load->view('templates/header');
-        $this->load->view('pages/'$page);
+        $this->load->view('pages/'$page, $data);
         $this->load->view('templates/footer');
       }
 
