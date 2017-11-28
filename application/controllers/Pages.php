5f116bddb0e13b76d6166bab3cfb2b8875bf4931
Zaladar <oscarjwhaglund@gmail.com>
2017-11-28 21:33:15 +0100

    CIframework installed

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -14,7 +14,7 @@
 
         $data['title'] = ucfirst($page); // Capitalize the first letter
 
-        $this->load->view('templates/headers', $data);
+        $this->load->view('templates/header', $data);
         $this->load->view('pages/'.$page, $data);
         $this->load->view('templates/footer', $data);
       }
