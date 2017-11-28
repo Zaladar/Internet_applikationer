ca140435972775acee7bdd92f824b3340e3dbcbe
Zaladar <oscarjwhaglund@gmail.com>
2017-11-28 17:30:34 +0100

    CIframework installed

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -1,5 +1,5 @@
 <?php
-    class Main extends CI_Controller {
+    class Pages extends CI_Controller {
       public function view($page="home"){
         if(!file_exists(APPPATH.'views/pages/'.$page.'.php'){
           show_404();
