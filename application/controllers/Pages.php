b536a65873408cd9e5b9136fe8a659ec88b630dc
Zaladar <oscarjwhaglund@gmail.com>
2017-11-28 18:36:46 +0100

    CIframework installed

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -1,16 +1,18 @@
 <?php
     class Pages extends CI_Controller {
       public function view($page = 'home'){
-        if(!file_exists(APPPATH.'views/pages/'.$page.'.php')){
-          show_404();
+        if ( ! file_exists(APPPATH.'views/pages/'.$page.'.php'))
+        {
+                // Whoops, we don't have a page for that!
+                show_404();
         }
-        $data['title'] = ucfirst($page);
 
-        $this->load->view('templates/header');
+        $data['title'] = ucfirst($page); // Capitalize the first letter
+
+        $this->load->view('templates/header', $data);
         $this->load->view('pages/'.$page, $data);
-        $this->load->view('templates/footer');
+        $this->load->view('templates/footer', $data);
       }
-      
 /*
   		public function calendar(){
         $this->load->view('header');
@@ -32,5 +34,5 @@
         $this->load->view('pancakes');
         $this->load->view('footer');
       }*/
-    }
-    ?>
+}
+?>
