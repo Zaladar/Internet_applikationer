77f9cdc6b1a75e4d5198e62f2bc95f453dca415c
Zaladar <oscarjwhaglund@gmail.com>
2017-11-28 17:00:43 +0100

    CIframework installed

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -8,7 +8,7 @@
 
         $this->load->view('templates/header');
         $this->load->view($page);
-        $this->load->view('templates/footer')
+        $this->load->view('templates/footer');
       }
 
   		public function calendar(){
