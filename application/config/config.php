24d6bc3f93d6741a6629a9d5c1033be9aeac8d86
Zaladar <oscarjwhaglund@gmail.com>
2017-11-30 21:29:11 +0100

    CIframework installed

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/index.php';
+$config['base_url'] = 'http://213.89.155.39/index.php';
 
 /*
 |--------------------------------------------------------------------------
