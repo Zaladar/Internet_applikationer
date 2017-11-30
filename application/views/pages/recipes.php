0edb528e45a2e3fce6c3c30fc957c363535dc10d
Zaladar <oscarjwhaglund@gmail.com>
2017-11-30 13:00:08 +0100

    CIframework installed

--- application/views/pages/recipes.php
+++ application/views/pages/recipes.php
@@ -1,26 +1,4 @@
-<br class="clr"/>
-<div id="register" class="modal">
-  <form class="modal-content animate" action="/Model/register.php" method="post">
-    <div class="container">
-      <label></label>
-      <input type="text" placeholder="Enter Username" name="username" required>
-      <label></label>
-      <input type="password" placeholder="Enter Password" name="password" required>
-      <button type="submit">Register</button>
-    </div>
-  </form>
-</div>
-<div id="login" class="modal">
-  <form class="modal-content animate" action="/Model/login.php" method="post">
-    <div class="container">
-      <label></label>
-      <input type="text" placeholder="Enter Username" name="username" required>
-      <label></label>
-      <input type="password" placeholder="Enter Password" name="password" required>
-      <button type="submit">Login</button>
-    </div>
-  </form>
-</div>
+
 <div>
 </div>
 <div>
@@ -28,8 +6,8 @@
     <div>
       <h2>These are the current recipies!</h2>
       <ul>
-        <li><a href="meatballs.php">Meatballs!</a> </li>
-        <li><a href="pancakes.php">Pancakes!</a> </li>
+        <li><a href="<?php echo base_url(); ?>index.php/recipes/meatballs">Meatballs!</a> </li>
+        <li><a href="<?php echo base_url(); ?>index.php/recipes/pancakes">Pancakes!</a> </li>
       </ul>
     </div>
   </div>
