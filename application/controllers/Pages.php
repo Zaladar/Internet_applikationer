9908d16ac9016c3e374761e4eb05a19d1658b3e1
Zaladar <oscarjwhaglund@gmail.com>
2017-11-30 12:54:00 +0100

    CIframework installed

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -10,6 +10,7 @@
         if ( ! file_exists(APPPATH.'views/pages/'.$page.'.php'))
         {
                 // Whoops, we don't have a page for that!
+                echo "hello world!";
                 show_404();
         }
 
