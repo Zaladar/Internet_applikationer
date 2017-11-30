7d347ef39c11db1f5b8eb47e26a58560f75b45cf
Zaladar <oscarjwhaglund@gmail.com>
2017-11-30 15:40:21 +0100

    CIframework installed

--- application/views/users/register.php
+++ application/views/users/register.php
@@ -1,14 +1,15 @@
-<div id="register" class="modal">
-  <h2><?= $title ?></h2>
-  <?php echo validation_errors(); ?>
 
-  <?php echo form_open('users/register', 'class="modal-content animate"'); ?>
-  	<div class="container">
-          <label></label>
-  				<input type="text"  name="username" placeholder="Enter Username">
-  				<label></label>
-  				<input type="password" name="password" placeholder="Enter Password">
-  			<button type="submit">Register</button>
-  	</div>
-  <?php echo form_close(); ?>
-</div>
+<?php echo validation_errors(); ?>
+
+<?php echo form_open('users/register'); ?>
+	<div class="row">
+		<div class="col-md-4 col-md-offset-4">
+			<h1 class="text-center"><?= $title; ?></h1>
+        <label></label>
+				<input type="text" class="form-control" name="username" placeholder="Enter Username">
+				<label></label>
+				<input type="password" class="form-control" name="password" placeholder="Enter Password">
+			<button type="submit" class="btn btn-primary btn-block">Submit</button>
+		</div>
+	</div>
+<?php echo form_close(); ?>
