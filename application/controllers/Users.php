05f8be8d0bb590cebc7046fd9e0169b63e58e195
Zaladar <oscarjwhaglund@gmail.com>
2017-11-30 15:12:14 +0100

    CIframework installed

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -8,7 +8,7 @@ class Users extends CI_Controller{
     $this->form_validation->set_rules('password', 'Password', 'required');
     if($this->form_validation->run() === FALSE){
       $this->load->view('templates/header');
-      $this->load->view('index.php/users/register', $data);
+      $this->load->view('users/register', $data);
       $this->load->view('templates/footer');
     } else {
       // Encrypt password
