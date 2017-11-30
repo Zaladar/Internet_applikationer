892143ae730a0be420ac6fe98bcb6eafd193ad57
Zaladar <oscarjwhaglund@gmail.com>
2017-11-30 15:59:17 +0100

    CIframework installed

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -18,8 +18,8 @@
 			// Validate
 			$this->db->where('username', $username);
 			$result = $this->db->get('user');
-      $row = $result->$row();
-      if(password_verify($password,$row['password'])&&$result->num_rows() == 1){
+      $row = $result->row();
+      if(password_verify($password,$row['password'])&&($result->num_rows() == 1))){
   				return $result->row(0)->ID;
 			}
       else {return false;}
