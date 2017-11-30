d4778a980fe3ae288a9e79b4af193e4b22cce3e1
Zaladar <oscarjwhaglund@gmail.com>
2017-11-30 17:22:27 +0100

    CIframework installed

--- application/views/users/login.php
+++ application/views/users/login.php
@@ -1,26 +1,11 @@
 <?php echo form_open('users/login', 'class="modal-content animate"' ); ?>
-<div class="row">
-  <div class="col-md-4 col-md-offset-4">
-    <h1 class="text-center"><?php echo $title; ?></h1>
-    <div class="form-group">
-      <input type="text" name="username" class="form-control" placeholder="Enter Username" required autofocus>
-    </div>
-    <div class="form-group">
-      <input type="password" name="password" class="form-control" placeholder="Enter Password" required autofocus>
-    </div>
-    <button type="submit" class="btn btn-primary btn-block">Login</button>
+  <div class="container">
+    <h1><?php echo $title; ?></h1>
+      <label></label>
+      <input type="text" name="username" placeholder="Enter Username" required autofocus>
+      <label></label>
+      <input type="password" name="password" placeholder="Enter Password" required autofocus>
+    <button type="submit">Login</button>
   </div>
-</div>
 
-<div class="modal">
-  <form class="modal-content animate" action="/Model/login.php" method="post">
-    <div class="container">
-        <label></label>
-        <input type="text" placeholder="Enter Username" name="username" required autofocus>
-        <label></label>
-        <input type="password" placeholder="Enter Password" name="password" required autofocus>
-      <button type="submit">Login</button>
-    </div>
-  </form>
-</div>
 <?php echo form_close(); ?>
