2d081bd93760f22610aec73aa8a3e1b5242283be
Zaladar <oscarjwhaglund@gmail.com>
2017-11-30 17:27:26 +0100

    CIframework installed

--- application/views/users/login.php
+++ application/views/users/login.php
@@ -1,10 +1,10 @@
 <?php echo form_open('users/login', 'class="modal-content animate"' ); ?>
   <div class="container">
     <h1><?php echo $title; ?></h1>
-      <label></label>
-      <input type="text" name="username" placeholder="Enter Username" required autofocus>
-      <label></label>
-      <input type="password" name="password" placeholder="Enter Password" required autofocus>
+    <label></label>
+    <input type="text" name="username" placeholder="Enter Username" required autofocus>
+    <label></label>
+    <input type="password" name="password" placeholder="Enter Password" required autofocus>
     <button type="submit">Login</button>
   </div>
 
--- application/views/users/register.php
+++ application/views/users/register.php
@@ -2,14 +2,12 @@
 <?php echo validation_errors(); ?>
 
 <?php echo form_open('users/register'); ?>
-	<div class="row">
-		<div class="col-md-4 col-md-offset-4">
-			<h1 class="text-center"><?= $title; ?></h1>
-        <label></label>
-				<input type="text" class="form-control" name="username" placeholder="Enter Username">
-				<label></label>
-				<input type="password" class="form-control" name="password" placeholder="Enter Password">
-			<button type="submit" class="btn btn-primary btn-block">Submit</button>
-		</div>
+	<div class="container">
+		<h1 class="text-center"><?= $title; ?></h1>
+	  <label></label>
+		<input type="text" name="username" placeholder="Enter Username">
+		<label></label>
+		<input type="password" name="password" placeholder="Enter Password">
+		<button type="submit">Submit</button>
 	</div>
 <?php echo form_close(); ?>
