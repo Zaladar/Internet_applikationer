2e76db44f2bd4d21140576a1b7b2e428c23fb097
Zaladar <oscarjwhaglund@gmail.com>
2017-12-01 18:05:03 +0100

    CIframework installed

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,7 +50,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['recipes'] = 'recipes/index';
-$route['recipes/(:any)'] = 'recipes/view';
+$route['recipes/(:any)'] = 'recipes/view$1';
 
 $route['default_controller'] = 'pages/view';
 $route['(:any)'] = 'pages/view/$1';
