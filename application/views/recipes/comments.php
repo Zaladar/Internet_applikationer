f0c9b0bcdbbb54636879304c5ef74f54c2f77543
Zaladar <oscarjwhaglund@gmail.com>
2017-12-01 17:36:20 +0100

    CIframework installed

--- application/views/recipes/comments.php
+++ application/views/recipes/comments.php
@@ -39,23 +39,3 @@
 </div>
 </div>
 <br class="clr"/>
-<!--<?php
-include 'init/config.php';
-session_start();
-$sql = "SELECT ID, username, recipie, comment FROM comments WHERE recipie='meatballs'";
-$result = $conn->query($sql);
-// output data of each row
-
-while($row = $result->fetch_assoc()) {
-  echo '<div class="comments">';
-  echo '<div class="ucomment">';
-  echo ('<small class="name">'.$row['username'].'</small>');
-  if($_SESSION['login_user']== $row['username']){
-    echo(' ');}
-  echo '<br class="clr"/>';
-  echo ('<p class="comment">'.$row['comment'].'</p>');
-  echo'</div>';
-  echo'</div>';
-}
-$conn->close();
-?>
