ec61965160b0b32a2a50b65515faf130f0f520ed
Zaladar <oscarjwhaglund@gmail.com>
2017-12-01 19:30:47 +0100

    CIframework installed

--- application/views/recipes/comments.php
+++ application/views/recipes/comments.php
@@ -7,16 +7,12 @@
     <div class="ucomment">
       <small class="name"> <?php echo $comment['username']; ?> </small>
         <?php if( ($this->session->userdata('logged_in'))&&($this->session->userdata('username')==$comment['username'])) : ?>
-        <div class="comments ">
-          <div class="ucomment">
             <?php echo form_open('recipes/delete'); ?>
             <form>
             <input type="hidden" name="id" value="<?php echo $comment['ID']; ?>">
             <button type="submit" class="delete">&#9851</button>
             </form><br/>
             <?php echo form_close(); ?>
-          </div>
-        </div>
         <?php endif; ?>
         <br class="clr"/>
       <p class="comment"> <?php echo $comment['comment']; ?> </p>
