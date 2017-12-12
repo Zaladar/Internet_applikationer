edd0ce924d2349af9b8f345bf82d21cbf4f05114
Zaladar <oscarjwhaglund@gmail.com>
2017-12-12 22:04:38 +0100

    CIframework installed

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,6 +2,7 @@
   <head>
       <title>Tasty Recipes</title>
       <link rel = "stylesheet" type = "text/css" href = "/css/stylesheet.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   </head>
   <body>
     <header>
