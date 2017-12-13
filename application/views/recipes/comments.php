a9a9ee3a67415d108f5e5f0b288b74eab086cda5
Zaladar <oscarjwhaglund@gmail.com>
2017-12-14 00:28:17 +0100

    CIframework installed

--- application/views/recipes/comments.php
+++ application/views/recipes/comments.php
@@ -3,7 +3,7 @@
   <script src="<?php echo base_url();?>/js/comments.js"></script>
   <?php if($this->session->userdata('logged_in')): ?>
   <div class="comments">
-    <form action="<?php echo base_url().'recipes/delete'.$recipe?>" method="POST">
+    <form action="<?php echo base_url().'recipes/create/>'.$recipe?>" method="POST">
       <div class="ucomment">
         <label></label>
         <input class="comment" type="text" placeholder="Enter comment" name="comment" required autofocus>
