9f35c259570d292ab3c3e0207a96671a12b51dfc
Zaladar <oscarjwhaglund@gmail.com>
2017-12-14 00:30:25 +0100

    CIframework installed

--- application/views/recipes/comments.php
+++ application/views/recipes/comments.php
@@ -1,6 +1,58 @@
 <div class="column">
   <h3>Comments</h3>
-  <script src="<?php echo base_url();?>/js/comments.js"></script>
+  <script>
+  $(document).ready(function() { //remember to put document.ready function when you are using jquery then insert your jquery functions inside.
+    function commentEvent(target){// should be converted click -> submit
+      target.on('submit', function(e) {
+        e.preventDefault();
+        var that = $(this),
+        url = that.attr('action'),
+        type = that.attr('method'),
+        data = {};
+        that.find('[name]').each(function(index, value) {
+           var that = $(this),
+           name = that.attr('name'),
+           value = that.val();
+           data[name] = value;
+         });
+
+        $.ajax({
+        url: url,
+        type: type,
+        data: data,
+        success: function(response) {
+          $(that)[0].reset();
+          loadComments();
+          }
+        });
+
+      });
+    });
+  eventHandler($('form.ajax'));
+  function loadComments(e){
+    $.ajax({
+        type: "ajax",
+        url: "<?php echo base_url();?>recipes/retrieve/<?php echo $recipe ?>",
+        async: true,
+        dataType: 'json',
+        success: function (result) {
+            //var obj = $.parseJSON(result);
+            var output='';
+            for(var i=0; i< result.length;i++){
+              output += '<div class="comments "><div class="ucomment"><small class="name">'+ result[i].username + '</small>';
+              if( '<?php echo $this->session->userdata('username') ;?>' == result[i].username){
+                output += '<form action="<?php echo base_url();?>recipes/delete/' + result[i].ID + '" method="POST">' + '<button type="submit" class="delete">&#9851</button></form>';
+              }
+              output += '</br><br class="clr"/><p class="comment">' + result[i].comment + '</p></div></div>';
+            }
+            $('#comments').html(output);
+            cEvent($('#comments form.ajax'));
+        }
+    })
+  };
+  });
+  </script>
+
   <?php if($this->session->userdata('logged_in')): ?>
   <div class="comments">
     <form action="<?php echo base_url().'recipes/create/'.$recipe?>" method="POST">
