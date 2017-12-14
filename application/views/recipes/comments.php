195003ccea933c98be9d8a75fd845c2b8c2d947a
Zaladar <oscarjwhaglund@gmail.com>
2017-12-14 21:17:06 +0100

    CIframework installed

--- application/views/recipes/comments.php
+++ application/views/recipes/comments.php
@@ -1,68 +1,8 @@
 <div class="column">
   <h3>Comments</h3>
   <div id="comments"></div>
-  <script>
-  $(document).ready(function(){
-  function commentEvent(target){
-  target.on('submit', function(e) {
-    e.preventDefault();
-    var that = $(this),
-    url = that.attr('action'),
-    type = that.attr('method'),
-    data = {};
-    // Fetches all the data from the forms parts that has a name attribute and saves them.
-    that.find('[name]').each(function(index, value) {
-      var that = $(this),
-      name = that.attr('name'),
-      value = that.val();
-      data[name] = value;
-    });
+  <script src="/js/comments.js">
 
-    // Send data to the targeted function to handle it.
-    $.ajax({
-      url: url,
-      type: type,
-      data: data,
-      success: function(response) {
-        $(that)[0].reset();
-        showComments();
-      }
-    });
-  });
-  return false;
-};
-
-// Add first event listerner
-commentEvent($('form'));
-
-function showComments(e){
-  $.ajax({
-    type: 'ajax',
-    url: "<?php echo base_url();?>recipes/retrieve",
-    async: true,
-    dataType: 'json',
-    success: function(result){
-      var output = '';
-      for(var i=0; i< result.length;i++){
-        output += '<div class="comments "><div class="ucomment"><small class="name">'+ result[i].username + '</small>';
-        if( '<?php echo $this->session->userdata('username') ;?>' == result[i].username){
-          output += '<form action="<?php echo base_url();?>recipes/delete/' + result[i].ID + '" method="POST"><button type="submit" class="delete">&#9851</button></form>';
-        }
-        output += '</br><br class="clr"/><p class="comment">' + result[i].comment + '</p></div></div>';
-      }
-      // Replace the old content of #comments with the newly fetched comments.
-      $('#comments').html(output);
-      // Add event listerner to the new comments.
-      commentEvent($('#comments form'));
-    },
-    error: function(){
-      alert('could not get data from database');
-    }
-  })
-  return false;
-};
-showComments();
-  });
   </script>
 
   <?php if($this->session->userdata('logged_in')): ?>
--- js/comments.js
+++ js/comments.js
@@ -1,56 +1,61 @@
-<script>
-
-
-$(document).ready(function() { //remember to put document.ready function when you are using jquery then insert your jquery functions inside.
-  function commentEvent(target){// should be converted click -> submit
-    target.on('submit', function(e) {
-      e.preventDefault();
-      var that = $(this),
-      url = that.attr('action'),
-      type = that.attr('method'),
-      data = {};
-      that.find('[name]').each(function(index, value) {
-         var that = $(this),
-         name = that.attr('name'),
-         value = that.val();
-
-         data[name] = value;
-       });
-
-      $.ajax({
-      url: url,
-      type: type,
-      data: data,
-      success: function(response) {
-        $(that)[0].reset();
-        loadComments();
-        }
-      });
-    });
-    return false;
+$(document).ready(function(){
+function commentEvent(target){
+target.on('submit', function(e) {
+  e.preventDefault();
+  var that = $(this),
+  url = that.attr('action'),
+  type = that.attr('method'),
+  data = {};
+  // Fetches all the data from the forms parts that has a name attribute and saves them.
+  that.find('[name]').each(function(index, value) {
+    var that = $(this),
+    name = that.attr('name'),
+    value = that.val();
+    data[name] = value;
   });
-commentEvent($('form.ajax'));
 
-function loadComments(e){
+  // Send data to the targeted function to handle it.
   $.ajax({
-      type: 'ajax',
-      url: '<?php echo base_url();?>recipes/retrieve/<?php echo $recipe ?>',
-      async: true,
-      dataType: 'json',
-      success: function (result) {
-          var output='';
-          for(var i=0; i< result.length;i++){
-            output += '<div class="comments "><div class="ucomment"><small class="name">'+ result[i].username + '</small>';
-            if( '<?php echo $this->session->userdata('username'); ?>' == result[i].username){
-              output += '<form action="<?php echo base_url();?>recipes/delete/' + result[i].ID + '" method="POST">' + '<button type="submit" class="delete">&#9851</button></form>';
-            }
-            output += '</br><br class="clr"/><p class="comment">' + result[i].comment + '</p></div></div>';
-          }
-          $('#comments').html(output);
-          commentEvent($('#comments form.ajax'));
-      }
+    url: url,
+    type: type,
+    data: data,
+    success: function(response) {
+      $(that)[0].reset();
+      showComments();
+    }
   });
-  return false;
-};
 });
-</script>
+return false;
+};
+
+// Add first event listerner
+commentEvent($('form'));
+
+function showComments(e){
+$.ajax({
+  type: 'ajax',
+  url: "<?php echo base_url();?>recipes/retrieve",
+  async: true,
+  dataType: 'json',
+  success: function(result){
+    var output = '';
+    for(var i=0; i< result.length;i++){
+      output += '<div class="comments "><div class="ucomment"><small class="name">'+ result[i].username + '</small>';
+      if( '<?php echo $this->session->userdata('username') ;?>' == result[i].username){
+        output += '<form action="<?php echo base_url();?>recipes/delete/' + result[i].ID + '" method="POST"><button type="submit" class="delete">&#9851</button></form>';
+      }
+      output += '</br><br class="clr"/><p class="comment">' + result[i].comment + '</p></div></div>';
+    }
+    // Replace the old content of #comments with the newly fetched comments.
+    $('#comments').html(output);
+    // Add event listerner to the new comments.
+    commentEvent($('#comments form'));
+  },
+  error: function(){
+    alert('could not get data from database');
+  }
+})
+return false;
+};
+showComments();
+});
