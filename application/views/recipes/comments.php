35e84d443da88b06f73354fbbc87b4ac7041e32d
Zaladar <oscarjwhaglund@gmail.com>
2017-12-14 15:26:33 +0100

    CIframework installed

--- application/views/recipes/comments.php
+++ application/views/recipes/comments.php
@@ -38,7 +38,7 @@
 };*/
 
 function dasEvent(target){// should be converted click -> submit
-  target.on('submit',function(e) {
+  target.submit(function(e) {
     e.preventDefault();
     var that = $(this),
     url = that.attr('action'),
@@ -64,7 +64,6 @@ function dasEvent(target){// should be converted click -> submit
   return false;
 };
 dasEvent($('form.ajax'));
-  loadComments();
 function loadComments(e){
   $.ajax({
       type: "ajax",
@@ -76,8 +75,8 @@ function loadComments(e){
           var output='';
           for(var i=0; i< result.length;i++){
             output += '<div class="comments "><div class="ucomment"><small class="name">'+ result[i].username + '</small>';
-            if( '<?php echo $this->session->userdata('username') ;?>' == result[i].username){
-              output += '<form action="<?php echo base_url();?>recipes/delete/' + result[i].ID + '" method="POST">' + '<button type="submit" class="delete">&#9851</button></form>';
+            if( '<?php echo $this->session->userdata('username'); ?>' == result[i].username){
+              output += '<form action="<?php echo base_url(); ?>recipes/delete/' + result[i].ID + '" method="POST">' + '<button type="submit" class="delete">&#9851</button></form>';
             }
             output += '</br><br class="clr"/><p class="comment">' + result[i].comment + '</p></div></div>';
           }
